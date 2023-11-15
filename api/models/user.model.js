55dcd2c42250b85960d58f2bbc421b623c1396c2
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2023-11-15 21:29:25 +0530

    register functionality complete

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -42,3 +42,4 @@ const userSchema = new Schema({
 });
 
 export default mongoose.model("User",userSchema)
+
--- api/server.js
+++ api/server.js
@@ -9,11 +9,13 @@ import orderRoute from "./routes/order.route.js"
 import reviewRoute from "./routes/review.route.js"
 import authRoute from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 const app = express()
 dotenv.config()
 mongoose.set("strictQuery", true)
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 
 
 const connetc = async ()=>{
@@ -36,6 +38,9 @@ app.use((err,req,res,next)=>{
     const errorStatus = err.status || 500
     const errorMessage = err.message || "something went wrong"
     return res.status(errorStatus).send(errorMessage)
+})
+app.use("/",(req,res)=>{
+    res.send("running")
 })
 app.listen(8080,()=>{
     connetc()
